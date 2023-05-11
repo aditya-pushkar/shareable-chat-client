e29d9e676d7065045fa7542623c4d7036cb2515f
aditya-pushkar <adityapushkar850@gmail.com>
2023-05-11 16:38:47 +0530

    tailwind with dasyui --setup

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import RootLayout from './component/RootLayout'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <RootLayout>
+    </RootLayout>
+  )
+}
+
+export default App
