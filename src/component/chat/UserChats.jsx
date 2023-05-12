1900b5e2f8932e39b6264c2a84353edb5cfe0e07
aditya-pushkar <adityapushkar850@gmail.com>
2023-05-13 03:11:30 +0530

    user-chats-list & user-fork-chats-list compo. --added

---
+++ src/component/chat/UserChats.jsx
@@ -0,0 +1,54 @@
+import React from "react";
+
+const UserChats = () => {
+  return (
+    <>
+      <li>
+        <a>How to land on mars</a>
+      </li>
+      <li>
+        <a>How to land on mars and </a>
+      </li>
+      <li>
+        <a>How to land on mars</a>
+      </li>
+      <li>
+        <a>How to land on mars</a>
+      </li>
+      <li>
+        <a>How to land on mars</a>
+      </li>
+      <li>
+        <a>How to land on mars</a>
+      </li>
+      <li>
+        <a>How to land on mars</a>
+      </li>
+      <li>
+        <a>How to land on mars</a>
+      </li>
+      <li>
+        <a>How to land on mars</a>
+      </li>
+      <li>
+        <a>How to land on mars</a>
+      </li><li>
+        <a>How to land on mars</a>
+      </li><li>
+        <a>How to land on mars</a>
+      </li><li>
+        <a>How to land on mars</a>
+      </li><li>
+        <a>How to land on mars</a>
+      </li><li>
+        <a>How to land on mars</a>
+      </li>
+      <li>
+        <a>How to land on mars and</a>
+      </li>
+      
+    </>
+  );
+};
+
+export default UserChats;
---
+++ src/component/chat/UserForkChats.jsx
@@ -0,0 +1,54 @@
+import React from 'react'
+
+const UserForkChats = () => {
+  return (
+    <>
+      <li>
+        <a>How To Fork a Project</a>
+      </li>
+      <li>
+        <a>How To Fork a Project</a>
+      </li>
+      <li>
+        <a>How To Fork a Project</a>
+      </li>
+      <li>
+        <a>How To Fork a Project</a>
+      </li>
+      <li>
+        <a>How To Fork a Project</a>
+      </li>
+      <li>
+        <a>How To Fork a Project</a>
+      </li>
+      <li>
+        <a>How To Fork a Project</a>
+      </li>
+      <li>
+        <a>How To Fork a Project</a>
+      </li>
+      <li>
+        <a>How To Fork a Project</a>
+      </li>
+      <li>
+        <a>How To Fork a Project</a>
+      </li><li>
+        <a>How To Fork a Project</a>
+      </li><li>
+        <a>How To Fork a Project</a>
+      </li><li>
+        <a>How To Fork a Project</a>
+      </li><li>
+        <a>How To Fork a Project</a>
+      </li><li>
+        <a>How To Fork a Project</a>
+      </li>
+      <li>
+        <a>How To Fork a Project and</a>
+      </li>
+      
+    </>
+  )
+}
+
+export default UserForkChats
